8dc0b4e7416edfe63ffa0d8d5ba4e4231bc55510
Andreas Rauch <51051984+Psycr0w@users.noreply.github.com>
2022-07-24 19:11:13 +0200

    Add files via upload

---
+++ YoutubeMp3.py
@@ -0,0 +1,59 @@
+﻿import os
+import shutil
+from pytube import YouTube
+
+if os.name == 'nt':
+    import ctypes
+    from ctypes import windll, wintypes
+    from uuid import UUID
+
+    # ctypes GUID copied from MSDN sample code
+    class GUID(ctypes.Structure):
+        _fields_ = [
+            ("Data1", wintypes.DWORD),
+            ("Data2", wintypes.WORD),
+            ("Data3", wintypes.WORD),
+            ("Data4", wintypes.BYTE * 8)
+        ] 
+
+        def __init__(self, uuidstr):
+            uuid = UUID(uuidstr)
+            ctypes.Structure.__init__(self)
+            self.Data1, self.Data2, self.Data3, \
+                self.Data4[0], self.Data4[1], rest = uuid.fields
+            for i in range(2, 8):
+                self.Data4[i] = rest>>(8-i-1)*8 & 0xff
+
+    SHGetKnownFolderPath = windll.shell32.SHGetKnownFolderPath
+    SHGetKnownFolderPath.argtypes = [
+        ctypes.POINTER(GUID), wintypes.DWORD,
+        wintypes.HANDLE, ctypes.POINTER(ctypes.c_wchar_p)
+    ]
+
+    def _get_known_folder_path(uuidstr):
+        pathptr = ctypes.c_wchar_p()
+        guid = GUID(uuidstr)
+        if SHGetKnownFolderPath(ctypes.byref(guid), 0, 0, ctypes.byref(pathptr)):
+            raise ctypes.WinError()
+        return pathptr.value
+
+    FOLDERID_Download = '{374DE290-123F-4565-9164-39C4925E467B}'
+
+    def get_download_folder():
+        return _get_known_folder_path(FOLDERID_Download)
+else:
+    def get_download_folder():
+        home = os.path.expanduser("~")
+        return os.path.join(home, "Downloads")
+
+URL = str(input("URL:- "))
+yt = YouTube(URL)
+video = yt.streams.filter(only_audio=True).first()
+downloaded_file = video.download()
+base, ext = os.path.splitext(downloaded_file)
+new_file = base + '.mp3'
+new_file_name = new_file.split("\\")[-1]
+os.rename(downloaded_file, new_file)
+shutil.move(new_file, get_download_folder() + '\\' + new_file_name)
+print(new_file_name)
+print("Done")
